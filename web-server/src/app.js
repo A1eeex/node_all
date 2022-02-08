360fc3005ba481765ac8675a8264938c873cae0c
Alex <opetrovskyi@mggpaero.com>
2022-02-08 12:15:12 +0100

    add hbr module

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,33 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const PORT = 3000;
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
-    res.send('<h1>Hello express</h1>')
-})
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Alex',
-        age: 29,
-    },
-        {
-            name: "Jef",
-            age: 30,
-        }])
+    res.render('index', {
+        title: 'index app',
+        name: 'alex'
+    })
 })
 app.get('/about', (req, res) => {
-    res.send(`<h1>about title</h1>`)
+    res.render('about', {
+        name: 'alex'
+    })
 })
 app.get('/weather', (req, res) => {
     res.send('Weather page')
 })
 
-app.listen(5000, () => {
-    console.log('server work')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'help Page'
+    })
+})
+app.listen(PORT, () => {
+    console.log('server work on port ' + PORT)
 })
