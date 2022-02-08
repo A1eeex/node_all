7ebf88d3706ac664252b44216a69786130862b71
Alex <opetrovskyi@mggpaero.com>
2022-02-08 13:24:39 +0100

    fix hbr(component work) registerPartials (corect write with last leater's')

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const PORT = 3000;
+
+// Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars engine and views location
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../templates/views'));
+hbs.registerPartials(partialsPath)
+// hbs.registerPartial(partialsPath)
+// Setup static directory to save
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -14,11 +23,13 @@ app.get('', (req, res) => {
         name: 'alex'
     })
 })
+
 app.get('/about', (req, res) => {
     res.render('about', {
         name: 'alex'
     })
 })
+
 app.get('/weather', (req, res) => {
     res.send('Weather page')
 })
@@ -28,6 +39,7 @@ app.get('/help', (req, res) => {
         title: 'help Page'
     })
 })
+
 app.listen(PORT, () => {
     console.log('server work on port ' + PORT)
 })
